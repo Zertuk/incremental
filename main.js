9418ec03705c9fecd474fe1e47c971683b2dcc43
Justin <zertukis@gmail.com>
2014-05-10 23:28:48 -0500

    base store to/from functionality added

--- main.js
+++ main.js
@@ -1,18 +1,54 @@
 var ectoplasm = 0;
+var ghostStoreVal = false;
 
 function ectoplasmClick(number) {
-	ectoplasm = ectoplasm + number;
-	document.getElementById('ectoplasm').innerHTML = ectoplasm;
+	ectoplasm = ectoplasm + number*10;
+	document.getElementById('ectoplasm').innerHTML = "You have " + ectoplasm + " stuff";
 }
 
 function ectoplasmGenerator(num) {
 	ectoplasm = ectoplasm + num;
-	document.getElementById('ectoplasm').innerHTML = ectoplasm;
+	document.getElementById('ectoplasm').innerHTML = "You have " + ectoplasm + " stuff";
+}
+
+function ghostStore()  {
+	if (ectoplasm > 99) {
+		ghostStoreVal = true;
+		document.getElementById('storeButton').style.display = "inline";
+		return ghostStoreVal;
+	}
+	return ghostStoreVal;
+}
+
+function leaveStore() {
+	store.style.display = "none";
+	main.style.display = "inline";
+}
+
+window.onload = function() {
+
+var storeButton = document.getElementById('storeButton');
+var store = document.getElementById('store');
+var main = document.getElementById('main');
+
+storeButton.addEventListener('click', function() {
+	store.style.display = "inline";
+	main.style.display = "none";
+})
 
 }
 
+
 window.setInterval(function() {
 
-	ectoplasmGenerator(5);
+	ectoplasmGenerator(50);
 
-}, 1000);
+	if (ghostStoreVal == false){
+	ghostStore();
+	}
+
+
+
+
+}, 500);
+
