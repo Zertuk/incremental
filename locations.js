3ca14c9f0a87efffc04a01010e26c68e4ffc3bd9
Justin <zertukis@gmail.com>
2014-05-18 04:36:45 -0500

    most of the camp scenario functions setup

--- locations.js
+++ locations.js
@@ -28,6 +28,36 @@ function enterMapLocation(mapLocation) {
 		}
 	}
 }
+var campCount = 0;
+
+function searchCamp() {
+	$('#steal_items').show();
+	$('#camp_text').html('There is lots of swag here, but no people still');
+}
+var thief = false;
+function stealItems() {
+	$('#error').html('You take the items and leave, gained 5000 ectoplasm, 100 blood');
+	blood = blood + 100;
+	ectoplasm = ectoplasm + 5000;
+	enterMapLocation('#campground');
+	thief = true;
+}
+function campgroundWait() {
+	if (campCount == 0) {
+		$('#camp_text').html('You waited for thirty minutes and no one showed up');
+	}
+	else if (campCount == 1) {
+		$('#camp_text').html('Still no one around, you look around and see a lot of supplies');
+	}
+	else {
+		$('#camp_text').html('You may now use the campers tent to rest, free of charge')
+		$('#man').show();
+		$('#man_text').show();
+		$('#camp_scenario').hide();
+		$('#camp_use').show();
+	}
+	campCount++;
+}
 
 var demon = false;
 var demonFirst = false;
