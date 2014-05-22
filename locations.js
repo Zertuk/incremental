65e3b5bb3a5880da355176c1cf87ad6a1e742af4
Justin <zertukis@gmail.com>
2014-05-22 01:23:01 -0500

    removed the old unneccesary functions

--- locations.js
+++ locations.js
@@ -1,31 +1,9 @@
-var mapShow = false;
-var mainShow = true;
-var trainShow = false;
-
-function enterMapLocation(mapLocation) {
-	if (mapShow == false) {
-		$(mapLocation).hide();
-		$('#map').show();
-		mapShow = true;
+function trainTicket() {
+	if (inventoryObject.ticket == true) {
+		$('#trainStation').toggle();
 	}
 	else {
-		$(mapLocation).show();
-		$('#map').hide();
-		mapShow = false;
-		if (mapLocation == '#mountain') {
-			magicDoor();
-		}
-		if (mapLocation == '#trainShow') {
-			if (inventoryObject.ticket == true) {
-				$('#trainStation').show();
-				trainShow = true;
-			}
-			else {
-				$('#error').html('You need a ticket to ues that');
-				$('#map').show();
-				$(mapLocation).hide();
-			}
-		}
+		$('#error').html('You need a ticket to ues that');
 	}
 }
 
@@ -167,34 +145,4 @@ function enterDemon() {
 function usePool() {
 	$('#poolChoice').css('display', 'none');
 	$('#poolYes').css('display', 'inline');
-}
-
-function enterMountain() {
-
-}
-//any location from the main screen is displayed using this function
-//hides main if mainShow is true and brings up whichever location is choosen
-//otherwise hides the location and brings up main if location is shown
-//updates inventory and store status text and mapShow if approproiate
-function enterMainLocation(location) {
-	if (mainShow == true) {
-		$(location).show();
-		$('#main').hide();
-		mainShow = false;
-		if (location == '#store') {
-			$('#store_status').html('You looking to buy?');
-		}
-		else if (location == '#inventory') {
-			inventoryList();
-		}
-		else if (location == '#map') {
-			mapShow = true;
-		}
-	}
-	else {
-		$(location).hide();
-		$('#main').show();
-		mainShow = true;
-	}
-	error.innerHTML = '';
 }
