341b5732656088e7f05373d2719f93d9b91c2d97
Justin <zertukis@gmail.com>
2014-05-29 01:27:27 -0500

    added a few missing semicolons oops

--- levelObject.js
+++ levelObject.js
@@ -13,7 +13,7 @@ approach.specialMonster = demonWizard;
 approach.levelLength = 60;
 approach.specialMonsterNum = 3;
 approach.text = 'There are demons everywhere!';
-approach.area = '#churchInside'
+approach.area = '#churchInside';
 
 var cavern = new levelInfo();
 cavern.name = 'cavern';
@@ -27,7 +27,8 @@ mine.name = 'mine';
 mine.monster = goblinMiner;
 mine.specialMonster = demon;
 mine.specialMonster = 5;
-mine.specialMonster = 'Goblin miners are flooding the halls'
+mine.specialMonster = 'Goblin miners are flooding the halls';
+mine.area = '#mountain';
 
 var depths = new levelInfo();
 depths.name = 'depths';
@@ -36,6 +37,7 @@ depths.specialMonster = demonWizard;
 depths.specialMonsterNum = 1;
 depths.text = 'The bottom of the mine';
 depths.levelLength = 60;
+depths.area = '#mountain';
 
 var base = new levelInfo();
 base.name = 'base';
@@ -44,7 +46,7 @@ base.specialMonster = vampire;
 base.levelLength = 55;
 base.specialMonsterNum = 1;
 base.text = 'The base of the tower';
-
+base.area = '#tower';
 
 var upper = new levelInfo();
 upper.name = 'base';
@@ -53,22 +55,25 @@ upper.specialMonster = vampire;
 upper.levelLength = 53;
 upper.specialMonsterNum = 1;
 upper.text = 'The upper level of the tower, the top is near!';
-
+upper.area = '#tower';
 
 var top = new levelInfo();
 top.name = 'top';
 top.monster = skeleton;
+top.monsterNum = 4;
 top.specialMonster = reaper;
 top.specialMonsterNum = 1;
 top.levelLength = 55;
-top.text = 'The sun is rising in the distance'
+top.text = 'The sun is rising in the distance';
+top.area = '#tower';
 
 var cave = new levelInfo();
 cave.name = 'cave';
 cave.monster = bear;
 cave.specialMonster = dropBear;
 cave.monsterNum = 2;
-cave.text = 'Inside a bears cave! ...Is this a good idea?'
+cave.text = 'Inside a bears cave! ...Is this a good idea?';
+cave.area = '#cavern';
 
 var den = new levelInfo();
 den.name = 'den';
@@ -78,7 +83,7 @@ den.levelLength = 36;
 den.text = 'The heart of the bears den! Whats that house doing here?';
 den.specialMonsterNum = 1;
 den.monsterNum = 2;
-
+cave.area = '#cavern';
 
 var levelObject = {
 	approach: approach,
