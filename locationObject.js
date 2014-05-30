e67faf050f14e7f7e7a64d7fee568151b98160f9
Justin <zertukis@gmail.com>
2014-05-29 21:56:11 -0500

    starting to convert all of the locations to an object, made animate function for the ascii as well

---
+++ locationObject.js
@@ -0,0 +1,119 @@
+var button;
+var oldButtons ='';
+function locationObject() {
+	this.questText = 'A default text display here',
+	this.ascii = '|===|===|',
+	this.text = '',
+	this.buttons = '-',
+	this.name = 'default location',
+	this.specButtons = '',
+	this.animate = 500,
+	this.createLocButton = function(buttonClass, to, from, type) {
+		this.buttons = '<button class = "' + buttonClass + '"' + ' value = "' + to + ',' + from + '">' + type + '</button>';
+		this.displayButton();
+	}
+	this.displayButton = function() {
+		console.log(oldButtons);
+		$('#testLoc').html(this.buttons + oldButtons);
+		oldButtons = this.buttons +''+ oldButtons;
+		console.log(this.buttons);
+	}
+	this.createSelect = function() {
+		this.select = '<select id = "' + questId + '">';
+	}
+$('.location_button, pre').click(function() {
+		var buttonValue = $(this).attr('value');
+		var split = buttonValue.split(',');
+		console.log('test');
+		$('#' + split[1]).hide();
+		$('#' + split[0]).fadeIn('slow');
+		if (split[0] == 'mountain') {
+			magicDoor();
+		}
+		else if (split[0] == 'trainShow') {
+			trainTicket();
+		}
+		else if (split[0] == 'store') {
+			$('#store_status').html('You looking to buy?');
+		}
+		else if (split[0] == '#inventory') {
+			inventoryList();
+		}
+		error.innerHTML = '';
+	});
+}
+
+function animateAscii() {
+	$('#location_ascii').toggle();
+	$('#location_ascii2').toggle();
+	console.log('hellllo');
+
+	setTimeout(animateAscii, 500);
+}	
+		
+
+
+var Store = new locationObject();
+Store.ascii = "\n\
+      ////^\\\\\\\\\n\
+      | ^   ^ |\n\
+     @ (o) (o) @\n\
+      |   v   |\n\
+      |  ___  |\n\
+       \\_____/\n\
+     ____|  |____\n\
+    /    \\__/    \\\n\
+   /              \\\n\
+  /\\_/|        |\\_/\\\n\
+ / /  |        |  \\ \\\n\
+(  |  |        |   > )\n\
+ \\ \\  |        |  / /\n\
+  \\ \\ |________| / /\n\
+   \\ \\|_I_D_I__|/ /\n\
+    \\ \\ / I  \\ / /\n\
+     \\ /  I   \\ /\n\
+     |         |\n\
+     |    |    |\n\
+     |    |    |\n\
+     |    |    |\n\
+     |    |    |\n\
+     | ## | ## |\n\
+     |    |    |\n\
+     |    |    |\n\
+     |____|____|\n\
+     (____(____)\n\
+      _| | _| |\n\
+  cccC__Cccc___)\n\ ";
+
+Store.ascii2 = "\n\
+      ////^\\\\\\\\\n\
+      | ^   ^ |\n\
+     @ -=- -=- @\n\
+      |   v   |\n\
+      |  ___  |\n\
+       \\_____/\n\
+     ____|  |____\n\
+    /    \\__/    \\\n\
+   /              \\\n\
+  /\\_/|        |\\_/\\\n\
+ / /  |        |  \\ \\\n\
+(  |  |        |   > )\n\
+ \\ \\  |        |  / /\n\
+  \\ \\ |________| / /\n\
+   \\ \\|_I_D_I__|/ /\n\
+    \\ \\ / I  \\ / /\n\
+     \\ /  I   \\ /\n\
+     |         |\n\
+     |    |    |\n\
+     |    |    |\n\
+     |    |    |\n\
+     |    |    |\n\
+     | ## | ## |\n\
+     |    |    |\n\
+     |    |    |\n\
+     |____|____|\n\
+     (____(____)\n\
+      _| | _| |\n\
+  cccC__Cccc___)\n\ ";
+
+Store.text = 'You looking to buy?';
