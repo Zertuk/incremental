445a0135fd285b48947d30124241b1745507b5e7
Justin <zertukis@gmail.com>
2014-06-01 19:39:52 -0500

    border now changes on inverse and the hp font color stays white always so it is readable

--- options.js
+++ options.js
@@ -3,11 +3,14 @@ function inverseColors() {
 	if (inverse == false) {
 		$('body').css('color', 'black');
 		$('body').css('background-color', 'white');
+		$('#ectoplasmDiv').css('border-color', 'black');
+		$('#hp').css('color', 'white');
 		inverse = true;
 	}
 	else {
 		$('body').css('color', 'white');
 		$('body').css('background-color', 'black');
+		$('#ectoplasmDiv').css('border-color', 'white');
 		inverse = false;
 	}
 }
