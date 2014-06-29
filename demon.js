fc8391b5a2e57f5572c5cf9ddb4d5d48b96cbce2
Justin <zertukis@gmail.com>
2014-06-28 23:48:35 -0500

    converted all values in demon file to new object format, also deleted some old stuff that wasnt needed

--- demon.js
+++ demon.js
@@ -1,35 +1,4 @@
-var speedSpell = false;
-var berserkSpell = false;
-var fleshSpell = false;
-var moneySpell = false;
-var envySpell = false;
-var expSpell = false;
-var prideSpell = false;
-var sinChoosen = false;
-
-function speedSpell() {
-
-}
-
-function endBerserk() {
-	berserk = false;
-	player.damage = player.damage * 0.5;
-	console.log(player.damage);
-}
-
-function useBerserk() {
-	if (potionUsed) {
-		$('#error').html('Spells are on Cooldown!');
-	}
-	else if (potionUsed == false) {
-		player.damage = player.damage*2;
-		console.log(player.damage)
-		potionCD = 10;
-		berserk = true;
-		setTimeout(endBerserk, 5000);
-	}
-}
-
+//takes players choice and asks them to confirm, then applies their choice affects and player.sinChoosen becomes true for the monk encounter
 function chooseSin(choice) {
 	var message = "You can only pick one and thats what you are going with?";
 	if (confirm(message)) {
@@ -43,34 +12,31 @@ function chooseSin(choice) {
 				$('#error').html('You choose Gluttony. You heal faster');
 				break;
 			case "greed":
-				ectoplasm = ectoplasm * 2;
-				moneySpell = true;
+				player.money = player.money * 2;
 				$('#error').html('You choose Greed. You gain extra money');
 				break;
 			case "sloth":
-				seedsPlanted = seedsPlanted * 5;
-				batteriesUsed = batteriesUsed * 5;
+				player.gears = player.gears * 5;
+				player.batteries = player.batteries * 5;
 				$('#error').html('You choose Sloth. You generate resources faster');
 				break;
 			case "wrath":
-				swordEnchantVal = swordEnchantVal + 0.25;
+				player.swordEnchantVal = player.swordEnchantVal + 0.25;
 				$('#error').html('You choose Wrath. You deal more damage')
 				break;
 			case "envy":
 				player.freedom = player.freedom + 0.1;
-				batteriesUsed = batteriesUsed * 2;
-				seedsPlanted = seedsPlanted * 2;
-				swordEnchantVal = swordEnchantVal + 0.075
-				armorEnchantVal = armorEnchantVal + 0.075;
+				player.batteries = player.batteries * 2;
+				player.gears = player.gears * 2;
+				player.swordEnchantVal = player.swordEnchantVal + 0.075
+				player.armorEnchantVal = player.armorEnchantVal + 0.075;
 				$('#error').html('You choose Envy. You get a little of everything')
 				break;
 			case "pride":
-				armorEnchantVal = armorEnchantVal + 0.25;
+				player.armorEnchantVal = player.armorEnchantVal + 0.25;
 				$('#error').html('You choose pride. You can take more damage now')
 				break;
 		}
 		player.sinChoosen = true;
-
-
 	}
 }
